0843d60d38a0900d5ee3e94229e2e9526e4987b0
liguo <li.guo@alivexbiotech.com>
2020-07-02 15:21:20 +0800

    modify

--- node-demo-02/app.js
+++ node-demo-02/app.js
@@ -3,8 +3,8 @@ const  koa = require('koa')
 
 let app = new koa()
 
-app.use(async  ctx=>{
-    console.log("ctx")
+app.use("/api/users",async  ctx=>{
+    console.log(ctx)
     ctx.body = "你好,koa2.x"
 })
 
