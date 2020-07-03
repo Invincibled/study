81909166e54d121468619a8f7b8f473f258c314d
liguo <li.guo@alivexbiotech.com>
2020-07-03 18:34:11 +0800

    add

---
+++ typescript-demo-01/index.js
@@ -0,0 +1,12 @@
+console.log("ffff");
+var str = "111";
+var str1 = "2222";
+var str3 = "333";
+var str4 = "fadfa";
+var Flag;
+(function (Flag) {
+    Flag[Flag["success"] = 1] = "success";
+    Flag[Flag["error"] = -1] = "error";
+})(Flag || (Flag = {}));
+var num;
+console.log(num);
---
+++ typescript-demo-01/index.ts
@@ -0,0 +1,12 @@
+let str:string = "111"
+let str1:string ="2222"
+let str3:string = "333"
+
+let  str4="fadfa"
+
+enum Flag {success = 1 , error = -1}
+
+
+var num:number|null|undefined;
+
+console.log(num)
