af68b507f73120d71a5b41fb007206eb92f5a018
liguo <li.guo@alivexbiotech.com>
2020-08-06 14:43:27 +0800

    nested router

--- angular-demo07/src/app/app.module.ts
+++ angular-demo07/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { PcateComponent } from './components/product/pcate/pcate.component';
+import { PlistComponent } from './components/product/plist/plist.component';
+import { WelcomeComponent } from './components/home/welcome/welcome.component';
+import { SettingComponent } from './components/home/setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    PcateComponent,
+    PlistComponent,
+    WelcomeComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
