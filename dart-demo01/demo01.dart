ca6750af801fae678a12a269c7f1ef0ba6691c6e
liguo <li.guo@alivexbiotech.com>
2020-08-12 12:50:35 +0800

    add

---
+++ dart-demo01/demo01.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+void main() {
+  print("hello world");
+
+  var list = new List();
+  list.add(1);
+
+  min(11, 22);
+}
---
+++ dart-demo01/demo02.dart
@@ -0,0 +1,16 @@
+void main() {
+  var str1 = "hello world";
+  print(str1);
+
+  String str2 = "你好";
+  print(str2);
+  String str3 = "fdaf";
+  int f1 = 13;
+  print("$str3 $f1");
+
+  const PI = 3.15;
+  print(PI);
+
+  final LA = 234;
+  print(LA);
+}
