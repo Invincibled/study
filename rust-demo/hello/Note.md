298a1c9de8901cc1c8674c86e4632c8ce143c08e
liguo <liguo@liguos-MacBook-Pro.local>
2020-08-28 22:42:01 +0800

    add

---
+++ rust-demo/hello/Note.md
@@ -0,0 +1,3 @@
+1. cargo check 检查
+2. cargo build 构建
+3. cargo run 运行
---
+++ rust-demo/hello/src/main.rs
@@ -0,0 +1,38 @@
+// const MAX_POINT : u32 = 10000;
+
+fn main() {
+    // let a =1 ;
+    // println!("a={}", a);
+
+    // let mut b = 1;
+    // println!("b={}", b);
+
+    // b = 2;
+    // println!("b={}",b);
+    // println!("Hello, world!");
+
+    // println!("{}",MAX_POINT);
+
+
+    //bool
+    // let is_true: bool = true;
+
+    // let is_false = false;
+
+    // println!("{} {}", is_true, is_false);
+
+    // char 是32位的,可以是个字符，也可以是汉字.
+    // let a = 'a';
+
+    // let b ="你";
+
+    // println!("{} {}", a, b)
+
+    // 自适应类型，isize,usize.在不同的平台上，所支持的不一样，比如64位，32位等等
+
+    // println!("{}", usize::max_value())
+
+    // 数组.
+
+    
+}
